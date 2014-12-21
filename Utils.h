f83c9a26e5fffa3be6eba7072a208ffaa534e7e7
QuentinDuval <senmenty@gmail.com>
2014-12-21 15:50:03 +0100

    Added help algorithms

--- Utils.h
+++ Utils.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <algorithm>
 #include <assert.h>
 #include <sstream>
 #include <string>
@@ -21,6 +22,18 @@ namespace algorithm
    void generate(Container& cont, ValueType const& val, NextFct next)
    {
       generate(begin(cont), end(cont), val, next);
+   }
+
+   template<typename Container, typename Predicate>
+   typename Container::const_iterator findIf(Container const& cont, Predicate pred)
+   {
+      return std::find_if(begin(cont), end(cont), pred);
+   }
+
+   template<typename Container, typename Lesser>
+   typename Container::const_iterator minBy(Container const& cont, Lesser less)
+   {
+      return std::min_element(begin(cont), end(cont), less);
    }
 
    template<typename T>
