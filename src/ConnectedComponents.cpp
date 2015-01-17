a694a8ac3173eef8746f65b00c7172fb665b73d9
QuentinDuval <senmenty@gmail.com>
2015-01-17 15:32:21 +0100

    Generalize ConnectedComponents

--- src/ConnectedComponents.cpp
+++ src/ConnectedComponents.cpp
@@ -4,21 +4,6 @@
 
 namespace algorithm
 {
-   ConnectedComponents::ConnectedComponents(Graph const& g)
-      : m_componentCount(0)
-      , m_componentIds(g.vertexCount(), 0)
-   {
-      DFS<Edge> dfs(g);
-      for (size_t i = 0; i < g.vertexCount(); ++i)
-      {
-         if (dfs.isMarked(i))
-            continue;
-
-         dfs.searchFrom(i, [this](size_t v) { m_componentIds[v] = m_componentCount; });
-         ++m_componentCount;
-      }
-   }
-
    size_t ConnectedComponents::componentCount() const
    {
       return m_componentCount;
--- src/ConnectedComponents.h
+++ src/ConnectedComponents.h
@@ -1,5 +1,5 @@
 #pragma once
-#include "Graph.h"
+#include "internal/GenericGraph.h"
 #include <vector>
 
 
@@ -8,8 +8,21 @@ namespace algorithm
    class ConnectedComponents
    {
    public:
-      ConnectedComponents(Graph const& g);
-      ~ConnectedComponents() = default;
+      template<typename Edge>
+      ConnectedComponents(GenericGraph<Edge> const& g)
+         : m_componentCount(0)
+         , m_componentIds(g.vertexCount(), 0)
+      {
+         DFS<Edge> dfs(g);
+         for (size_t i = 0; i < g.vertexCount(); ++i)
+         {
+            if (dfs.isMarked(i))
+               continue;
+
+            dfs.searchFrom(i, [this](size_t v) { m_componentIds[v] = m_componentCount; });
+            ++m_componentCount;
+         }
+      }
 
       size_t componentCount() const;
       size_t componentIdOf(size_t vertex) const;
