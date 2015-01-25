5cd5e90a5939092a62b17303e51ed28f9260dff1
QuentinDuval <senmenty@gmail.com>
2015-01-25 18:43:55 +0100

    Strongly connected components
    Factorization

--- src/ConnectedComponents.h
+++ src/ConnectedComponents.h
@@ -14,38 +14,22 @@ namespace algorithm
    {
    public:
       template<typename Edge>
-      ConnectedComponents(GenericGraph<Edge> const& g)
+      explicit ConnectedComponents(GenericGraph<Edge> const& g)
          : m_componentCount(0)
          , m_componentIds(g.vertexCount(), 0)
       {
          DFS<Edge> dfs(g);
-         for (size_t v : g.vertices())
-         {
-            if (dfs.isMarked(v))
-               continue;
-
-            dfs.markFrom(v, [this](size_t w) { m_componentIds[w] = m_componentCount; });
-            ++m_componentCount;
-         }
+         dfsInOrder(dfs, g.vertices());
       }
 
       template<typename Edge>
-      ConnectedComponents(GenericDiGraph<Edge> const& g)
+      explicit ConnectedComponents(GenericDiGraph<Edge> const& g)
          : m_componentCount(0)
          , m_componentIds(g.vertexCount(), 0)
       {
-         TopologicalSort order(g);
-
          GenericDiGraph<Edge> reversed = makeReversed(g);
-         DFS<Edge> reversedDfs(reversed);
-         for (auto v : order.order())
-         {
-            if (reversedDfs.isMarked(v))
-               continue;
-
-            reversedDfs.markFrom(v, [this](size_t w) { m_componentIds[w] = m_componentCount; });
-            ++m_componentCount;
-         }
+         DFS<Edge> dfs(reversed);
+         dfsInOrder(dfs, TopologicalSort(g).order());
       }
 
       size_t componentCount() const
@@ -61,6 +45,20 @@ namespace algorithm
       bool connected(size_t v, size_t w) const
       {
          return componentIdOf(v) == componentIdOf(w);
+      }
+
+   private:
+      template<typename Edge, typename VertexIt>
+      void dfsInOrder(DFS<Edge>& dfs, Range<VertexIt> const& range)
+      {
+         for (auto v : range)
+         {
+            if (dfs.isMarked(v))
+               continue;
+
+            dfs.markFrom(v, [this](size_t w) { m_componentIds[w] = m_componentCount; });
+            ++m_componentCount;
+         }
       }
 
    private:
