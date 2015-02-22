55092af0661cf7a2eb0153c927b0cd1af1bcd689
QuentinDuval <senmenty@gmail.com>
2015-02-22 13:50:08 +0100

    RBT

--- src/Set.h
+++ src/Set.h
@@ -241,29 +241,6 @@ namespace algorithm
          return insert_(nullptr, m_root, key);
       }
 
-      void erase(key_iterator it)
-      {
-         if (it == end())
-            return;
-
-         Node& node = *(it.getNode());
-         erase_(incomingLink(node), node);
-      }
-
-      void erase(key_iterator first, key_iterator last)
-      {
-         auto prev = begin();
-         auto start = std::distance(begin(), first);
-         for (decltype(start) i = 0; i < start - 1; ++i)
-            ++prev;
-
-         while (first != last)
-         {
-            erase(first);
-            first = start ? prev++ : begin();
-         }
-      }
-
    private:
       bool insert_(Node* father, std::unique_ptr<Node>& currentNode, Key const& key)
       {
@@ -282,36 +259,6 @@ namespace algorithm
             return insert_(currentNode.get(), currentNode->m_right, key);
          }
          return false;
-      }
-
-      void erase_(std::unique_ptr<Node>& incomingLink, Node& node)
-      {
-         //If two children, go right, sink all way down left, swap these nodes, then delete
-         if (node.m_right && node.m_left)
-         {
-            Node* nextHigher = Node::sinkLeft(node.m_right.get());
-            std::swap(nextHigher->m_value, node.m_value);
-            erase(key_iterator(nextHigher));
-            return;
-         }
-
-         //Otherwise, replace the incoming node from father:
-         Node* father = node.m_father;
-         if (node.m_left)
-         {
-            node.m_left->m_father = father;
-            incomingLink = move(node.m_left);
-         }
-         else if (node.m_right)
-         {
-            node.m_right->m_father = father;
-            incomingLink = move(node.m_right);
-         }
-         else
-         {
-            incomingLink.reset();
-         }
-         incrementCount_(father, -1);
       }
    };
 }
